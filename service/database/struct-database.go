52dce4e5420ba40714b30da5a8f9af3f5d2973df
Zoe Matrullo <104142441+zoematr@users.noreply.github.com>
2023-12-07 18:39:40 +0100

    basically re-did the api.yaml
    changed approach, some stuff still missing

--- service/database/struct-database.go
+++ service/database/struct-database.go
@@ -3,8 +3,14 @@ package database
 import "time"
 
 type Photo struct {
-	PhotoId  string      `json:"photoid"` 
-	UserId   string     `json:"owner"`
-	Likes    []UserId   `json:"likes"`
-	Date     time.Time  `json:"date"`     // Date in which the photo was uploaded
+	PhotoId    string
+	UserId     string
+	PhotoFile  string
+	ListLikes  []UserId
+	Date       time.Time
+}
+
+
+type UserId struct {
+	UserId     
 }
