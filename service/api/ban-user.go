2734e5656a122ff758edb4e243ebb891a51a6800
zoematr <zoe.matrullo@gmail.com>
2024-03-07 14:18:49 +0100

    runs but problem: ban returns always 404

--- service/api/ban-user.go
+++ service/api/ban-user.go
@@ -9,13 +9,14 @@ import (
 
 
 func (rt *_router) banUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-
+	ctx.Logger.Infof("start")
 	usernameRequestUser := ps.ByName("username")
 	tokenRequestUser, err := rt.db.GetTokenFromUsername(usernameRequestUser)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	ctx.Logger.Infof("found user from the path")
 	bearerToken := r.Header.Get("Authorization")
 	w.Header().Set("Content-Type", "application/json")
 	var usernameTargetUser string
@@ -25,22 +26,18 @@ func (rt *_router) banUser(w http.ResponseWriter, r *http.Request, ps httprouter
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
-	// Controlla se l'utente che effettua la richiesta ha il permesso di bannare l'utente specificato. 
-	// (solo l'owner dell'account puo aggiugnere un banned user nel suo account list)
 	allow := validateRequestingUser(tokenRequestUser, bearerToken)
 	if allow != 0 {
 		w.WriteHeader(allow)
 		return
 	}
 
-	// Controlla se l'utente sta cercando di bannare se stesso.se si(400:"Bad Request")
 	if usernameRequestUser == usernameTargetUser {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
-	// Chiama una funzione del database per aggiungere l'utente specificato alla lista degli utenti bannati.
+	
+	ctx.Logger.Infof("checks done, user not banned yet")
 	err = rt.db.BanUser(usernameRequestUser, usernameTargetUser)
 	if err != nil {
 		ctx.Logger.WithError(err).Error("ban user/db.BanUser: error executing insert query")
